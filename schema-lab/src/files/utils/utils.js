5d6e45620cbee1a3238007eda5aabeca87d6d1d9
tzerefos <tzerefos@athenarc.gr>
2025-05-13 13:44:46 +0300

    Move helper functions to utils file. Change timestamp format.

---
+++ schema-lab/src/files/utils/utils.js
@@ -0,0 +1,18 @@
+const timestampToDateOptions = {
+  year: "2-digit",
+  month: "2-digit",
+  day: "2-digit",
+  hour: "2-digit",
+  minute: "2-digit",
+  second: "2-digit",
+};
+
+const formatBytes = (bytes) => {
+  if (bytes === 0 || !bytes) return "0 B";
+  const k = 1024;
+  const sizes = ["B", "KB", "MB", "GB", "TB"];
+  const i = Math.floor(Math.log(bytes) / Math.log(k));
+  return parseFloat((bytes / Math.pow(k, i)).toFixed(2)) + " " + sizes[i];
+};
+
+export { timestampToDateOptions, formatBytes };
